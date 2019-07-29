ef8c17e33d4c8e4c878188cc0ad31dff6da3afbe
Dan Clipca <47941658+virtumonde@users.noreply.github.com>
2019-07-29 21:25:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,26 +4,20 @@ import Settings from './Settings'
 import {StyleSheet, View} from 'react-native'
 import Permissions from 'react-native-permissions'
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   }
 })
 
-
 export default class App extends React.Component {
     state = {
-      displayCam: true
-    }
+      displayCam: true }
 
-    // Switch to camera or settings
+    // Switch to camera or settings screen
     switchScreen = () => {
       this.setState( 
-        {displayCam: !this.state.displayCam}
-        )
-    }
+        {displayCam: !this.state.displayCam})}
     
   render() {
     return (
